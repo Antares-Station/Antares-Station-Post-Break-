72fdc9874d996126c2f855480e581a7152856485
Lucas F <lucasfreitas09876@gmail.com>
2018-03-14 20:34:58 -0400

    New Scene! Dynamic Cam System!
    Started designing first level and developed a system of triggers that change focus of cams based on player position.

---
+++ Assets/Script/CamTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamTrigger : MonoBehaviour {
+
+    public camerafollow cam;
+    public GameObject target;
+
+
+    public int ID;
+
+    private void Start()
+    {
+        cam = GameObject.Find("Main Camera").GetComponent<camerafollow>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("player"))
+        {
+            cam.target = target.transform;
+        }
+    }
+}
